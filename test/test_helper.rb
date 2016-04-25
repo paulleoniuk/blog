ec83a3193fba537c34396f3a8a73ab866843e4bb
xzibitrv <pashababak@gmail.com>
2016-04-25 21:57:58 +0300

    Make a basic User model(include secure password)

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,13 +1,15 @@
 ENV['RAILS_ENV'] ||= 'test'
+
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 require "minitest/reporters"
 Minitest::Reporters.use!
 
 class ActiveSupport::TestCase
+	include ApplicationHelper
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical
   # order.
-  fixtures :all
+    fixtures :all
 
   # Add more helper methods to be used by all tests here...
 end
