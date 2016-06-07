2c552c5b177f7e6c440e23914333d3db908415dd
xzibitrv <pashababak@gmail.com>
2016-06-08 01:59:35 +0300

    Add user microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ActiveRecord::Base
   before_save   :downcase_email
   before_create :create_activation_digest
 	validates :name, presence: true, length: {maximum: 50}
+  has_many :microposts, dependent: :destroy
 
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   	validates :email, presence: true, length: { maximum: 255 },
@@ -64,6 +65,10 @@ class User < ActiveRecord::Base
     reset_sent_at < 2.hours.ago
   end
 
+  def feed
+    Micropost.where("user_id = ?", id)
+  end
+  
   private
   
     # Converts email to all lower-case.
