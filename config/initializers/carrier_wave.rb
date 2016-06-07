ffcba63cf99249ae2094412e247262d553354fa3
xzibitrv <pashababak@gmail.com>
2016-06-08 02:24:25 +0300

    Region AWS S3 added

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -5,7 +5,7 @@
       # Configuration for Amazon S3
       :provider              => 'AWS',
       :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
-      :aws_secret_access_key => ENV['S3_SECRET_KEY']
+      :aws_secret_access_key => ENV['S3_SECRET_KEY'],
       :region 				 => "us-west-2"
     }
     config.fog_directory     =  ENV['S3_BUCKET']
